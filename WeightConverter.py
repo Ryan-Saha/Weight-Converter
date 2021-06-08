65e45b5c1409dca0a0e53040566a1c859446f0fa
Ryan-Saha <85521019+Ryan-Saha@users.noreply.github.com>
2021-06-08 11:11:57 -0600

    Update WeightConverter.py

--- WeightConverter.py
+++ WeightConverter.py
@@ -4,6 +4,6 @@ kg_or_lbs = input("(K)g or (L)bs: ")
 l = 'l'
 k = 'k'
 if kg_or_lbs == l:
-    print("Weight in Kg: " + str(float(weight) / 0.45))
+    print("Weight in Kg: " + str(float(weight) * 0.45))
 elif kg_or_lbs == k:
-    print("Weight in Lbs: " + str(float(weight) * 0.45))
+    print("Weight in Lbs: " + str(float(weight) / 0.45))
