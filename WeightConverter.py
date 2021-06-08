dda4ca0fcda07ff66a8c4259d7de2afabdd69c7b
Ryan-Saha <85521019+Ryan-Saha@users.noreply.github.com>
2021-06-07 22:33:10 -0600

    Create WeightConverter.py

---
+++ WeightConverter.py
@@ -0,0 +1,9 @@
+#when it asks for kg or lbs but lowercase l or k
+weight = input("Weight: ")
+kg_or_lbs = input("(K)g or (L)bs: ")
+l = 'l'
+k = 'k'
+if kg_or_lbs == l:
+    print("Weight in Kg: " + str(float(weight) / 0.45))
+elif kg_or_lbs == k:
+    print("Weight in Lbs: " + str(float(weight) * 0.45))
